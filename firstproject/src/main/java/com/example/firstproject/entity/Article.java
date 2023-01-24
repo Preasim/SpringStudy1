77f35f54bfa308ff58092bafa03a9d89b3f99af6
demuu <preasim.kr@gmail.com>
2023-01-25 04:37:13 +0900

    spring boot 17

--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -5,19 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity // DB가 해당 객체를 인식 가능
+@Entity // DB가 해당 객체를 인식 가능 (해당 클래스로 테이블을 만든다)
 @AllArgsConstructor
 @ToString
 @NoArgsConstructor
 @Getter
 public class Article {
     @Id // 대표값을 지정! like a 주민등록번호
-    @GeneratedValue // 1, 2, 3, ... 자동 생성 어노테이션
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id를 자동 생성하는 어노테이션
     private Long id;
     @Column
     private String title;
